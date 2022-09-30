bf6ed7c7fefb51e276ac5ca4f2d1a28700dfe9a8
Lucas <lucas2carlos@gmail.com>
2022-09-30 13:26:42 -0400

    adding Model

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+///////////////////////////// API ///////////////////////////
+
 const Api = (() => {
     const baseUrl = 'https://jsonplaceholder.typicode.com'
     // const baseUrl = 'https://localhost:4232'
@@ -24,3 +26,65 @@ const Api = (() => {
     }
     return {getTodos, deleteTodo, addTodo}
 })();
+
+
+///////////////////////////// VIEW ///////////////////////////
+
+const View = (() =>{
+    const dmstr = {
+        toDoContainer: '#toDoContainer',
+        dltBtn: ".dltBtn",
+        input: "#toDoInput",
+    }
+
+    const render = (ele, tmp) => {
+        ele.innerHTML = tmp
+    }
+
+    const createTmp = (arr) => {
+        let tmp = ""
+        for(let ele of arr){
+            tmp += `
+            <li>
+                <span>${ele.id} - ${ele.title}</span>
+                <button id="${ele.id}" clas="dltBtn">X</button>
+            </li>
+            `
+        }
+        return tmp
+    }
+
+    return {dmstr, render, createTmp}
+})();
+
+
+///////////////////////////// MODEL ///////////////////////////
+
+const Model = (() => {
+    const {getTodos, deleteTodo, addTodo} = api
+
+    class Todo {
+        constructor(title){
+            this.userId = 1;
+            this.title = title;
+            this.completed = false;
+        }
+    }
+
+    class State {
+        #todoList = [];
+
+        get todoList() {
+            return this.#todoList
+        }
+        set todoList(newTodoList) {
+            this.#todoList = newTodoList
+            const toDoContainer = document.querySelector(view.dmstr.toDoContainer)
+            const tmp = view.createTmp(this.#totodoList)
+            view.render(toDoContainer, tmp)
+        }
+    }
+
+    return {getTodos, deleteTodo, addTodo, State, Todo}
+
+})(Api, View)
